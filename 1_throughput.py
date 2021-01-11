0cdf3c23b81d87a109ea4e8aceb18e1444f4462f
Tianyuan <ftygary@gmail.com>
2021-01-12 00:02:25 +0800

    updated an automated testing script

--- 1_throughput.py
+++ 1_throughput.py
@@ -1,12 +1,36 @@
 from Runner import run
+import csv
 
 slas = [1, 5, 10, 20]
 cores = [1, 2, 4, 8, 16, 32, 64]
-batch_range = []
+batch_start = [2200, 2200, 3500, 6000, 9000, 15000, 17000]
 RATIO = 0.1
 EPOCH = 100
 
-# for sla in slas:
-#     for core in cores:
-#         run(2000, RATIO, EPOCH, sla, core)
-run(2000, RATIO, EPOCH, 1, 1)
+results_file = open("results", "w")
+write = csv.writer(results_file)
+
+for i in range(len(cores)):
+    for sla in slas:
+        core = cores[i]
+        print(core, sla)
+        batch_size_for_one_sec = batch_start[i]
+        current_batch = batch_size_for_one_sec * sla
+        increment = current_batch / 20
+
+        violation_counter = 0
+        next_exp = False
+
+        while not next_exp:
+            _1, _2, per_epoch_time = run(current_batch, RATIO, EPOCH, sla, core)
+            result = [core, sla, current_batch, per_epoch_time]
+            write.writerow(result)
+            print(result)
+            if per_epoch_time > sla:
+                violation_counter += 1
+            else:
+                violation_counter = 0
+            
+            if violation_counter > 3:
+                next_exp = True
+            current_batch += increment
