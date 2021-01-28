6ad17a0f561f82e0819b88e5778545773a9e7c29
Tianyuan <ftygary@gmail.com>
2021-01-28 18:49:04 +0800

    update system path

--- experiments/max_throughput/tensorflow_single_cpu/TensorflowSingleCPU.py
+++ experiments/max_throughput/tensorflow_single_cpu/TensorflowSingleCPU.py
@@ -1,5 +1,5 @@
 import os, sys
-sys.path.append('/Users/tywan/Documents/dev/streamlimit-utils')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))))
 
 from models.tensorflow.Lenet5 import Lenet5
 from stream_utils.ImageGenerator import *
